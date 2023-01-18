f88aa546a224843a0f313fae75a947534106b1cf
NaziraAGG <121957429+NaziraAGG@users.noreply.github.com>
2023-01-18 17:34:50 +0500

    new commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let name = "John"
+let admin = name
+
+console.log(admin)
+
+
+let balance = 25000
+let expense_car = 4480
+let expense_food = 890
+let expense = expense_car + expense_food
+let income_invoice = 5500
+let income_stock = 4200
+let income = income_invoice + income_stock
+let totalBalance = balance - expense + income
+console.log(totalBalance)
+
